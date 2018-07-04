2030a0baea73a3f8da7faa466a25a1835f872cc1
W. de Jong <wout.dejong@sanoma.com>
2018-07-04 14:57:45 +0200

    Change in config file so it can be added to gitignore file

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './features/language.feature'
+        './features/*.feature'
         // './features/shopping_bag.feature',
         // './features/productListPage.feature',
     ],
